093972eeceed1c6ac3d6c395ef323c13d59e69bd
Shubham-SK <kumar.shubham5504@gmail.com>
2020-06-04 04:45:42 +0000

    worked on shapefile correspondence - CSV file writing

--- shapefileCorrespondence.py
+++ shapefileCorrespondence.py
@@ -7,21 +7,21 @@ import geopandas as geopd
 import osgeo
 
 
-def readHE5(filename, dataset):
+def read_he5(he5_filename, dataset):
     """
     Returns a (720, 1440) Matrix containing requested data
     ---
-    filename: path to he5 file
+    he5_filename: path to he5 file
     dataset: requested data
     options: ColumnAmountNO2, ColumnAmountNO2CloudScreened,
              ColumnAmountNO2Trop, ColumnAmountNO2TropCloudScreened,
              Weight
     ---
-    Output Numpy array
+    Output Numpy Array
     """
-    with h5py.File(filename, "r") as f:
+    with h5py.File(he5_filename, "r") as f:
         # print(f['HDFEOS']['GRIDS']['ColumnAmountNO2']['Data Fields'].keys())
-        ds = f['HDFEOS']['GRIDS']['ColumnAmountNO2']['Data Fields'][dataset]
+        ds = np.array(f['HDFEOS']['GRIDS']['ColumnAmountNO2']['Data Fields'][dataset])
 
         # ds_no2 = np.array(f['HDFEOS']['GRIDS']['ColumnAmountNO2']['Data Fields']['ColumnAmountNO2'])
         # ds_cs = np.array(f['HDFEOS']['GRIDS']['ColumnAmountNO2']['Data Fields']['ColumnAmountNO2CloudScreened'])
@@ -34,41 +34,87 @@ def readHE5(filename, dataset):
         # print(f'ColumnAmountNO2Trop: {ds_not.shape}')
         # print(f'ColumnAmountNO2TropCloudScreened: {ds_tcs.shape}')
         # print(f'Weight: {ds_wht.shape}')
+
     return ds
 
 
-def readShapefile(filename, keyFieldName, dataset):
+def read_shape_file(shape_filename, key_field_name, dataset, temp_tiff):
     """
     Read shapefile and clean contents
     ---
-    filename: path to shp file
-    keyFieldName: column name (e.g. GID_0, NAME_0, FIPS etc.)
+    shape_filename: path to shp file
+    key_field_name: column name (e.g. GID_0, NAME_0, FIPS etc.)
+    dataset: Numpy Array (720, 1440) containing the grid data
+    temp_tiff: Temporary tiff dataset object for Rasterio compatibility
     ---
-    Output
+    Output Standard Array
     """
-    shpdata = geopd.GeoDataFrame.from_file(filename)
-    xyValues = [[keyFieldName, 'Max', 'Mean', 'Min']]
+    # read shape file and create output template
+    shp_data = geopd.GeoDataFrame.from_file(shape_filename)
+    xy_values = [[key_field_name, 'Max', 'Mean', 'Min']]
 
     # loop through the shape data
-    for i in range(len(shpdata)):
+    for i in range(len(shp_data)):
+
         # load the polygons into variables
-        geo = shpdata.geometry[i]
+        geo = shp_data.geometry[i]
         feature = [geo.__geo_interface__]
+
         # extract the requested column with using field name
-        itemindex = np.argwhere(shpdata.columns == keyFieldName)[0][0]
+        item_index = np.argwhere(shp_data.columns == key_field_name)[0][0]
+        name = shp_data.iloc[i][itemindex]
+
         # apply raster mask
-        out_image, out_transform = mask(dataset, feature, all_touched=True, crop=True, nodata=0)
-        if i == 1:
-            print(out_image)
-            print()
-            print(out_transform)
+        out_image, out_transform = mask(temp_tiff, feature, all_touched=True, crop=True, nodata=temp_tiff.nodata)
 
-    return shpdata
+        # extract values from out_image
+        values = out_image.tolist()
+        values = values[0]
+
+        # delete errors
+        min_data = dataset[~np.isnan(dataset)].min()
+        max_data = dataset[~np.isnan(dataset)].max()
+        out_data = []
+
+        for i in range(len(values)):
+            for j in range(len(values[k])):
+                if values[k][j] >= min_data and values[k][j] <= maxdata:
+                    out_data.append(values[k][j])
+
+        values = np.array(out_data)
+        maxes = values.max()
+        means = values.mean()
+        mins = values.min()
+
+        # add elements to output array
+        xy_values.append([name, maxes, means, mins])
+
+    # remove loop variables
+    del feature, shp_data
+
+    return shp_data
 
 
-ds = readHE5('OMI-Aura_L3-OMNO2d_2020m0518_v003-2020m0520t003149.he5', 'ColumnAmountNO2')
+def export_to_CSV(shape_filename, he5_filename, key_field_name, dataset, temp_tiff, lat_resolution, lon_resolution, csv_dir):
+    """
+    Creates CSV file with requested data
+    ---
+    shape_filename: path to shp file
+    key_field_name: column name (e.g. GID_0, NAME_0, FIPS etc.)
+    dataset: Numpy Array (720, 1440) containing the grid data
+    temp_tiff: Temporary tiff dataset object for Rasterio compatibility
+    lat_resolution: 0.25
+    lon_resolution: 0.25
+    csv_dir: directory to save csv files
+    """
+
+
+
+
+ds = read_he5('OMI-Aura_L3-OMNO2d_2020m0518_v003-2020m0520t003149.he5', 'ColumnAmountNO2')
 print(type(ds))
 # print(ds.shape)
 
-shp = readShapefile('USA_admin2/USA_admin2.shp', 'NAME_1', ds)
+shp = read_shape_file('USA_admin2/USA_admin2.shp', 'NAME_1', ds)
 print(shp)
+
