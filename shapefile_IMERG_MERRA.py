aa77982403b75a79162631e6fd80ee9e618c5aa5
Shubham-SK <kumar.shubham5504@gmail.com>
2020-06-10 23:09:08 +0000

    RUS_admin1 update

--- shapefile_IMERG_MERRA.py
+++ shapefile_IMERG_MERRA.py
@@ -165,6 +165,7 @@ if __name__ == "__main__":
     admin1_ITA = r'shapefiles/ITA_admin1/ITA_admin1.shp'
     admin1_Japan = r'shapefiles/Japan_admin1/Japan_admin1.shp'
     admin1_RUS = r'shapefiles/RUS_admin1/RUS_admin1.shp'
+    admin1_SWE = r'shapefiles/SWE_admin1/SWE_admin1.shp'
 
     # admin1_worldwide_top50_EU = r'F:\GMU-COVID-19\basemap_shp\basemap\admin1_worldwide(conti)\admin1_worldwide_top50_EU.shp'
     # admin0_worldwide = r'F:\GMU-COVID-19\basemap_shp\basemap\admin0_worldwide\admin0_worldwide.shp'
@@ -174,12 +175,12 @@ if __name__ == "__main__":
 
     # start calculation, save to CSV
     dataType='Humidity'
-    exportStatisticToCSV(admin1_RUS, inputRHRasterDir, 0.625, 0.5, 'daily_QV2M', 'nlon', 'nlat', 'HASC_1', dataType, r'RESULT_CSVS/', '2D', tempDir)
+    exportStatisticToCSV(admin1_SWE, inputRHRasterDir, 0.625, 0.5, 'daily_QV2M', 'nlon', 'nlat', 'HASC_1', dataType, r'RESULT_CSVS/', '2D', tempDir)
 
     dataType='Precipitation'
-    exportStatisticToCSV(admin1_RUS, inputPrecipRasterDir, 0.1, 0.1, 'daily_precipitation', 'nlon', 'nlat', 'HASC_1', dataType, r'RESULT_CSVS/', '2D', tempDir)
+    exportStatisticToCSV(admin1_SWE, inputPrecipRasterDir, 0.1, 0.1, 'daily_precipitation', 'nlon', 'nlat', 'HASC_1', dataType, r'RESULT_CSVS/', '2D', tempDir)
 
     dataType = 'Temper_T2MMEAN'
-    exportStatisticToCSV(admin1_RUS, inputTempRasterDir, 0.625, 0.5, 'daily_T2M', 'nlon', 'nlat', 'HASC_1', dataType, r'RESULT_CSVS/', '2D', tempDir)
+    exportStatisticToCSV(admin1_SWE, inputTempRasterDir, 0.625, 0.5, 'daily_T2M', 'nlon', 'nlat', 'HASC_1', dataType, r'RESULT_CSVS/', '2D', tempDir)
 
     print('Finish')
