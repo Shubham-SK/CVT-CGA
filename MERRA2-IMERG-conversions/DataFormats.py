b3270a1417c31e7967693262275dc23e8ccd0eda
Shubham Kumar <kumar.shubham5504@gmail.com>
2020-06-24 13:43:23 -0700

    Data Format Template

---
+++ MERRA2-IMERG-conversions/DataFormats.py
@@ -0,0 +1,12 @@
+from ConversionIO import *
+import numpy as np
+from tqdm import tqdm
+
+class sub_file_hourly:
+    def __init__(self):
+        pass
+
+
+class ext_file_hourly:
+    def __init__(self):
+        pass
